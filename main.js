9673dda17555e54e8a5dbfd827c72833175e41dc
srinadhrayudu <41734320+srinadhrayudu@users.noreply.github.com>
2018-07-27 16:13:00 +0530

    Add files via upload
    next commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function loadjson(file,callback){
 	var xhr=new XMLHttpRequest();
 	xhr.overrideMimeType("application/json");
 	xhr.open("GET",file,true);
-	xhr.onreadystatechange=function{
+	xhr.onreadystatechange=function(){
 		if (xhr.readyState===4 && xhr.status=="200"){
 			callback(xhr.responseText);
 		}
@@ -13,15 +13,16 @@ loadjson("data.json",function(text){
 	let data=JSON.parse(text);
 	console.log(data);
 	basic(data.details);
+	edu(data.education);
 })
 
-var main=document.queryselector(".main");
+var main=document.querySelector(".main");
 var left=document.createElement("div");
-left.classlist.add("left");
+left.classList.add("left");
 main.appendChild(left);
 function basic(basicdetails){
 var image=document.createElement("img");
-image.src=basicdetails.image
+image.src=basicdetails.image;
 left.appendChild(image);
 var name=document.createElement("h2");
 name.textContent=basicdetails.name;
@@ -32,4 +33,23 @@ left.appendChild(email);
 var phone=document.createElement("h2");
 phone.textContent=basicdetails.phone;
 left.appendChild(phone);
+}
+
+var right=document.createElement("div");
+right.classList.add("right");
+main.appendChild(right);
+function edu(education){
+var un=document.createElement("ul");
+right.appendChild(un);
+for (var i = 0; i < education.length; i++) {
+	var l=document.createElement("li");
+	l.textContent=education[i].course;
+	un.appendChild(l);
+	var l1=document.createElement("li");
+	l1.textContent=education[i].college;
+	un.appendChild(l1);
+	var l2=document.createElement("li");
+	l2.textContent=education[i].per;
+	un.appendChild(l2);
+}
 }
