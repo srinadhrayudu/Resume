a76a8cefbb691a9c90b6afe2fb839cf32369d649
srinadhrayudu <41734320+srinadhrayudu@users.noreply.github.com>
2018-07-28 14:19:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ function loadjson(file,callback){
 	xhr.overrideMimeType("application/json");
 	xhr.open("GET",file,true);
 	xhr.onreadystatechange=function(){
-		if (xhr.readyState===4 && xhr.status=="200"){
+		if (xhr.readyState ===4 && xhr.status =="200"){
 			callback(xhr.responseText);
+
 		}
 	}
 	xhr.send();
@@ -13,9 +14,10 @@ loadjson("data.json",function(text){
 	let data=JSON.parse(text);
 	console.log(data);
 	basic(data.details);
+	carr(data.carrier);
 	edu(data.education);
+	skill(data.skills);
 })
-
 var main=document.querySelector(".main");
 var left=document.createElement("div");
 left.classList.add("left");
@@ -39,10 +41,14 @@ var right=document.createElement("div");
 right.classList.add("right");
 main.appendChild(right);
 function edu(education){
+	var e=document.createElement("h1");
+	e.textContent="Educational Details";
+	right.appendChild(e);
+	e.appendChild(document.createElement("HR"))
 var un=document.createElement("ul");
 right.appendChild(un);
 for (var i = 0; i < education.length; i++) {
-	var l=document.createElement("li");
+	var l=document.createElement("h2");
 	l.textContent=education[i].course;
 	un.appendChild(l);
 	var l1=document.createElement("li");
@@ -53,3 +59,39 @@ for (var i = 0; i < education.length; i++) {
 	un.appendChild(l2);
 }
 }
+function skill(skilldata){
+	var s=document.createElement("div");
+	s.classList.add("sset");
+	right.appendChild(s);
+	var head=document.createElement("h1");
+	head.textContent="Skills set";
+	s.appendChild(head);
+	s.appendChild(document.createElement("HR"));
+	var t=document.createElement("table");
+	var tabledata="";
+	for (var i = 0; i < skilldata.length; i++) {
+	tabledata+="<tr><td>"+skilldata[i].title+"</td><td>"+skilldata[i].output+"</td></tr>";
+	t.innerHTML=tabledata;
+}
+head.appendChild(t);
+}
+
+function carr(carrier){
+	var d=document.createElement("div");
+	d.classList.add("carrier");
+	right.appendChild(d);
+	var e=document.createElement("h1");
+	e.textContent="Carrier Objective";
+	d.appendChild(e);
+	e.appendChild(document.createElement("HR"));
+	var para=document.createElement("p");
+	para.textContent=carrier.co;
+	e.appendChild(para);
+}
+
+
+
+
+
+
+
